7c9aa6233861f627cb7af60004e06d60f0f34ec1
daribar-http-whxyan <yan.n@densertech.com>
2026-03-19 13:11:31 +0500

    Add update.bat for downloading and running executable

---
+++ update.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001 >nul
+set BRANCH=%~1
+if "%BRANCH%"=="" set BRANCH=main
+set DIR=C:\Daribar\standartn-offline
+set REPO=https://raw.githubusercontent.com/daribar-http-whxyan/standartn-offline-loader
+timeout /t 3 /nobreak >nul
+curl.exe -Lo "%DIR%\standartn-offline.exe" "%REPO%/%BRANCH%/standartn-offline.exe"
+start "" "%DIR%\standartn-offline.exe"
+del "%~f0
