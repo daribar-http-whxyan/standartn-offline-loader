06d57a4ebb8d485d24dbd1400056eef1508eeb50
daribar-http-whxyan <yan.n@densertech.com>
2026-03-19 13:24:45 +0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,10 +1,11 @@
-@echo off
-chcp 65001 >nul
-set BRANCH=%~1
+@echo off                                                                                                                                                                                     
+chcp 65001 >nul 
+set BRANCH=%~1                                                                                                                                                                                
 if "%BRANCH%"=="" set BRANCH=main
 set DIR=C:\Daribar\standartn-offline
 set REPO=https://raw.githubusercontent.com/daribar-http-whxyan/standartn-offline-loader
 timeout /t 3 /nobreak >nul
 curl.exe -Lo "%DIR%\standartn-offline.exe" "%REPO%/%BRANCH%/standartn-offline.exe"
+for /d %%i in ("%TEMP%\_MEI*") do rd /s /q "%%i" >nul 2>&1
 start "" "%DIR%\standartn-offline.exe"
-del "%~f0
+del "%~f0"
